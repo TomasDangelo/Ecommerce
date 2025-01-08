99b54144d33d95f1ffc03688c9951f98cc0831dc
Tomas <tomasdangelo7@outlook.es>
2025-01-08 12:21:16 -0300

    Configurando models

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -1,4 +1,40 @@
 const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
-//const orderSchema 
+const orderModel = new Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    product: [
+        {
+            productId: {
+                type: String,
+                required: true
+            },
+            quantity: {
+                type: Number,
+                default: 1,
+            }
+        }
+    ],
+    amount: {
+        type: Number,
+        required: true,
+    },
+    status:{
+        type: String,
+        default: "pending",
+        enum: ["pending", "processing", "shipped", "delivered", "cancelled"]
+    },
+    address: {
+        type: Object,
+        required: true,
+    }
+},
+{
+    timestamps: true,
+}
+);
+
+module.exports = mongoose.model("Order", orderModel);
---
+++ server/models/productModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const productModel = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    categories: {
+        type: Array,
+        default: []
+    },
+    size: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+    color:{
+        type: String,
+
+    },
+
+},
+{
+    timestamps: true,
+}
+);
+
+module.exports = mongoose.model("Product", productModel);
